5d291280cb7b8951f4d865ef64923c39fe8110e2
Boris <boris.chang@cloud-interactive.com>
2024-03-13 15:28:28 +0800

    remove multiple lines swagger decorator build by tsup

--- generator/remove-swagger.ts
+++ generator/remove-swagger.ts
@@ -7,11 +7,11 @@ const fs = require('fs');
 
 function removeApiProperty(filePath: string) {
   let fileContent = fs.readFileSync(filePath, 'utf-8');
-   // 移除 import { ApiProperty } from '@nestjs/swagger' 及其後的換行符
-  fileContent = fileContent.replace(/import { ApiProperty } from '@nestjs\/swagger';\s*\n?/, '');
+  // 移除 import { ApiProperty } from '@nestjs/swagger' 及其後的換行符
+  fileContent = fileContent.replace(/import { ApiProperty } from ["']@nestjs\/swagger["'];\s*\n?/, '');
 
   // 移除 @ApiProperty decorators
-  fileContent = fileContent.replace(/@ApiProperty\(.*?\)\s*/g, '');
+  fileContent = fileContent.replace(/@ApiProperty\([\s\S]*?\)\s*/g, '');
 
   // console.log(chalk.blue('filePath: '), chalk.yellowBright(filePath));
 
